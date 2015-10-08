bc4dc0ca3a67391b35cd0d6e9d983695ca32bf44
MaximeMaillet <maxime.maillet93@gmail.com>
2015-10-08 22:32:57 +0200

    upgrade server + transmission object by socket

---
+++ Client/src/metier/Engine.java
@@ -0,0 +1,35 @@
+package metier;
+
+import ihm.graphic.MainView;
+import network.Connexion;
+import network.Message;
+
+public class Engine {
+
+	private static Engine instance;
+	private MainView mainframe;
+	private Engine()
+	{
+		
+	}
+	
+	public static Engine getInstance() {
+		if(instance == null)
+			instance = new Engine();
+		return instance;
+	}
+	
+	public void start(MainView m) {
+		this.mainframe = m;
+	}
+	
+	public void launchChat()
+	{
+		Connexion myConnexion = new Connexion();
+		while(true)
+		{
+			Message _m = myConnexion.receiveMessage();
+			this.mainframe.addMessage(_m);
+		}
+	}
+}
--- Server/src/network/ServerThread.java
+++ Server/src/network/ServerThread.java
@@ -2,9 +2,13 @@ package network;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 import java.net.Socket;
+
+import network.Message.TypeMessage;
 
 /**
 * Classe permettant d'accueilir dans un thread une nouvelle connexion au serveur
@@ -21,27 +25,21 @@ public class ServerThread extends Thread {
 	public void run() {
 		super.run();
 		
-		BufferedReader buffIN = null;
-		PrintStream buffOUT = null;
-		StringBuilder result = new StringBuilder();
-		
+		ObjectInputStream buffIN = null;
 		try {
-			while(true)
-			{
-				// Init buffers in/out
-				buffIN = new BufferedReader(new InputStreamReader(this.mySocket.getInputStream()));
-				buffOUT = new PrintStream(this.mySocket.getOutputStream());
+			buffIN = new ObjectInputStream(this.mySocket.getInputStream());
+			Message mess = (Message)buffIN.readObject();
+			if (mess != null) {
 				
-				System.out.println("Attente de valeurs");
-				String request = buffIN.readLine();
-				if(request == null || request.equals("END"))
-					break;
-				System.out.println(request);
+				if(mess.getType().equals(TypeMessage.LISTEN))
+					new ServerThreadListen(this.mySocket).start();
 				
-				
-				buffOUT.println("coucou");
-			}	
-		} catch (IOException e) {
+			}
+			else {
+				System.out.println("Message not received");
+			}
+			
+		} catch (IOException | ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
@@ -49,7 +47,6 @@ public class ServerThread extends Thread {
 		{
 			try {
 				buffIN.close();
-				buffOUT.close();
 				this.mySocket.close();
 				System.out.println("Client déconnecté");
 				
